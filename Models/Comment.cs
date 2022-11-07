70bab76e138c123a4f735d60375729b71838b402
Jeannette Geib <jeannettegeib@gmail.com>
2022-11-07 18:29:09 -0500

    saving for election day

---
+++ Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+        public int PostId { get; set; }
+        public string Message { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; } 
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string Bio { get; set; }
+
+        [Required] 
+        public DateTime DateCreated { get; set; }
+    }
+}
